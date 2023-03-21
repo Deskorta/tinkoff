eb532d889b1c47294ec618eef67021d7dd15f41d
Юлия Реутова <yulia.reutova@mail.ru>
2023-03-22 00:37:51 +0500

    commit 2

--- link-parser/src/main/java/ru/tinkoff/edu/java/linkParser/Main.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkParser/Main.java
@@ -1,7 +1,18 @@
-package ru.tinkoff.edu.java.linkParser;
-
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws MalformedURLException {
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String url = in.nextLine();
+
+
+
+        LinkParser githubParser = new GithubParser();
+        LinkParser stackOverflowParser = new StackOverflowParser();
+        githubParser.setNextParser(stackOverflowParser);
+
+        String[] result = githubParser.parse(url);
+        System.out.println(Arrays.toString(result));
     }
 }
